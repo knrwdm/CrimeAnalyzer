a34b51031ff361d6d84fee2a8996cdcdb9cae80b
Kayla Reap <42559069+knrwdm@users.noreply.github.com>
2019-04-05 20:15:02 -0500

    Create CrimeData.cs

---
+++ CrimeData.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CrimeAnalyzer
+
+{
+    public class CrimeData
+
+    {
+        public int Year { get; set; }
+        public int Population { get; set; }
+        public int Murders { get; set; }
+        public int Rapes { get; set; }
+        public int Robberies { get; set; }
+        public int ViolentCrimes { get; set; }
+        public int Thefts { get; set; }
+        public int MotorVehicleThefts { get; set; }
+    }
+
+}
